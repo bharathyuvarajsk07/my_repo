8cb74bfc3c04d4169049e61bc91dca1ca4e40a45
Bharath Yuvaraj S K <bharathyuvarajsk07@gmail.com>
2026-03-05 23:18:50 +0530

    Create s4.java

---
+++ CODECHEF/1.Basics/s4.java
@@ -0,0 +1,7 @@
+class Codechef {
+    public static void main(String[] args) {
+        
+        System.out.print("Hello, my name is Alex.");
+        System.out.print("Hello, my name is Bob.");
+    }
+}
