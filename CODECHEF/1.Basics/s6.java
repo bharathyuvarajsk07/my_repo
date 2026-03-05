a3332f637144fce883da7db6a8c971f39f6e54cd
Bharath Yuvaraj S K <bharathyuvarajsk07@gmail.com>
2026-03-05 23:26:40 +0530

    Create s6.java

---
+++ CODECHEF/1.Basics/s6.java
@@ -0,0 +1,15 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Integer literal for distance in kilometers
+        Integer i=100;
+        System.out.println(i);
+
+        // Float literal for distance in miles
+        Float f=62.14f;
+        System.out.println(f);
+
+        // Double literal for distance in light years
+        Double d=9.4607e12;
+        System.out.println(d);
+    }
+}
