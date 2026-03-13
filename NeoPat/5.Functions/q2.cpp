0200439c41a416e8267a3d27a7cc7cf19a66a733
Bharath Yuvaraj S K <bharathyuvarajsk07@gmail.com>
2026-03-14 01:52:43 +0530

    Create q2.cpp

---
+++ NeoPat/5.Functions/q2.cpp
@@ -0,0 +1,35 @@
+Single File Programming Question
+Create a program using a function that calculates the sum and product of two numbers.
+
+
+
+Function Prototype: 
+
+calc(int n1, int n2)
+
+Input format :
+The input consists of two integers, a and b, separated by a space.
+
+Output format :
+The output displays the sum and product of the given numbers.
+
+
+
+Refer to the sample output for the formatting specifications.
+
+Code constraints :
+-256 <= a, b <= 256
+
+Sample test cases :
+Input 1 :
+2 3
+Output 1 :
+Sum: 5
+Product: 6
+Input 2 :
+257 -257
+Output 2 :
+Invalid
+Whitelist
+Set 1:
+calc(int n1, int n2)
