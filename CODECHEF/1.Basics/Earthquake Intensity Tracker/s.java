55654cad60c939b52bb842a5a34131a389c2d598
Bharath Yuvaraj S K <bharathyuvarajsk07@gmail.com>
2026-03-26 23:18:24 +0530

    Create s.java

---
+++ CODECHEF/1.Basics/Earthquake Intensity Tracker/s.java
@@ -0,0 +1,17 @@
+class Codechef {
+    public static void main (String[] args) {
+        // 1. Declare regionOneIntensity as short and assign 12000
+        short regionOneIntensity = 12000;
+
+        // 2. Declare regionTwoIntensity as short and assign -5000
+        short regionTwoIntensity = -5000;
+
+        // 3. Declare regionThreeIntensity as byte and assign 0
+        byte regionThreeIntensity = 0;
+
+        // 4. Print all three values in the required format
+        System.out.println("Region One Intensity: " + regionOneIntensity);
+        System.out.println("Region Two Intensity: " + regionTwoIntensity);
+        System.out.println("Region Three Intensity: " + regionThreeIntensity);
+    }
+}
