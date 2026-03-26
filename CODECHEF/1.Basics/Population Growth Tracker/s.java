053fb3df6ca5e2e855386090b020e9014beb5027
Bharath Yuvaraj S K <bharathyuvarajsk07@gmail.com>
2026-03-26 23:20:02 +0530

    Create s.java

---
+++ CODECHEF/1.Basics/Population Growth Tracker/s.java
@@ -0,0 +1,13 @@
+class Codechef {
+    public static void main (String[] args) {
+        // 1. Declare currentPopulation as int and assign 1500000
+        int currentPopulation = 1500000;
+
+        // 2. Declare growthRate as int and assign 5000
+        int growthRate = 5000;
+
+        // 3. Print both values in the required format
+        System.out.println("Current Population: " + currentPopulation);
+        System.out.println("Annual Growth Rate: " + growthRate);
+    }
+}
