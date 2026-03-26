2e486de850ee1dd0ce70edb4ecc602c07ec311dc
Bharath Yuvaraj S K <bharathyuvarajsk07@gmail.com>
2026-03-26 23:10:04 +0530

    Time: 3 ms (67.27%) | Memory: 19.2 MB (77.58%) - LeetSync

---
+++ LEETCODE/69-sqrtx/sqrtx.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        
+        left, right = 2, x // 2
+        ans = 1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            num = mid * mid
+            
+            if num == x:
+                return mid
+            elif num < x:
+                ans = mid  # Store mid as a potential answer
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return ans
