e370d3236a46ea4c6142dc69c5057e40a59b3df8
skby-2005 <bharathyuvaraj264_bee27@mepcoeng.ac.in>
2026-04-01 12:21:22 +0530

    Create s.py

---
+++ HackerRank/String Formatting/s.py
@@ -0,0 +1,8 @@
+def print_formatted(number):
+    # your code goes here
+    width=len(bin(number))-2
+    for i in range(1,n+1):
+        print(str(i).rjust(width),
+        oct(i)[2:].rjust(width),
+        hex(i)[2:].upper().rjust(width),
+        bin(i)[2:].rjust(width))
